c118c5ac472082c3981aafa1716b13b4f6aeb4f2
Axel Guzmán <axl_bg@hotmail.com>
2024-04-05 18:00:50 -0300

    up

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const carousel = document.querySelector(".carousel");
 
+let isDragging = false;
+
+const dragStart = () => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+};
+
 const dragging = (e) => {
-  console.log(e.pageX);
+  if (!isDragging) return;
+  carousel.scrollLeft = e.pageX;
+};
+
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
 };
 
 carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mousedown", dragStart);
+document.addEventListener("mouseup", dragStop);
